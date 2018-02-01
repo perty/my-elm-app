7f698b11f3bf49119888d92b390141ec75de8216
Per Lundholm <per@artcomputer.se>
2018-02-01 13:16:32 +0100

    Fixed absolute reference to logo.svg. Now relative.

--- src/Main.elm
+++ src/Main.elm
@@ -36,7 +36,7 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
+        [ img [ src "logo.svg" ] []
         , h1 [] [ text "Your Elm App is working!" ]
         ]
 
